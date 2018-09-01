82424196da6e2b629a4a0b63ebcca4cc066d46ec
Lucero <luuceroo@gmail.com>
2018-08-31 20:51:58 -0500

    cambian urk db

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost:3327',
 	'username' => 'root',
 	'password' => 'mysql',
 	'database' => 'happyelder',
