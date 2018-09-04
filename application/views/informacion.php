fe95300b512a4738a5f70e0d5e9cb9befdb1e8e7
Lucero1308 <luuceroo01@gmail.com>
2018-09-04 15:31:04 -0500

    Update informacion.php

--- application/views/informacion.php
+++ application/views/informacion.php
@@ -1,11 +1,35 @@
 
+<hr class="featurette-divider">
 <h3>¿Quiénes somos?</h3>
+<hr class="featurette-divider">
 <p>En Happy Elder es una página web que brinda servicios profesionales para el cuidado de nuestros ancianos. Ofrecemos todo tipo de servicios de calidad, ya sean a domicilios, paseos rutinarios, actividades rutinarias, recreaciones terapéuticas ¡y más! Happy Elder quiere hacerte fácil lo que parecía difícil, te ayudaremos a encontrar a la mejor cuidadora de mayores para tu familiar. Queremos darte la tranquilidad de que tu familiar está en buenas manos.</p>
+<hr class="featurette-divider">
 <h3>¿Qué ofrecemos?</h3>
+<hr class="featurette-divider">
 <h5>CUIDADO DE ANCIANOS</h5>
 <p>Cuando una persona se hace mayor, a menudo necesita mayores atenciones en su día a día. El objetivo de Happy Elder es que las personas mayores puedan tener una mejor calidad de vida, haciéndolas sentir seguras.</p>
 <h5>EVENTOS PRO-VEJEZ:</h5>
 <p>Aprovechamos los tiempos libres y la energía vital de los jóvenes para canalizar actividades recreativas tratando de reinventar la vejez a base de la diversión. Por eso, se generarán eventos cada fin de semana para que sus adultos mayores puedan distraerse.</p>
-<div class="m-auto w-75 text-center mb-3">
-	<img src="<?= base_url( 'uploads/informacion.jpg' ) ?>" class="img-fluid">
-</div>
+<hr class="featurette-divider">
+<h2 align="center">Entendemos tus necesidades</h2>
+<h2 align="center">Ayudamos a las personas mayores con sus tareas del día a día</h2>
+<hr class="featurette-divider">
+ <div class="row">
+          <div class="col-lg-4" align ="center">
+            <img class="rounded-circle" src="<?= base_url( 'uploads/I1.png' ) ?>" alt="Generic placeholder image" width="140" height="140">
+            <h2>CUIDADO PERSONAL</h2>
+            <p>Aseo diario, higienes, movilizaciones y otras tareas</p>
+           
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4" align ="center">
+           <img class="rounded-circle" src="<?= base_url( 'uploads/I2.png' ) ?>" alt="Generic placeholder image" width="140" height="140">
+            <h2>ACOMPAÑAMIENTO</h2>
+            <p>Paseo, citas médicas, recados y compañía</p>
+         </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4" align ="center">
+             <img class="rounded-circle" src="<?= base_url( 'uploads/I3.png' ) ?>" alt="Generic placeholder image" width="140" height="140">
+            <h2>CURAS BÁSICAS</h2>
+            <p>Control de la medicación, insulina, cambios posturales, etc.</p>
+           </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+       
